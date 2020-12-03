cfe0375b58729386ee450bc3e95ddeb65b9f82bb
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-03 11:54:08 -0500

    Remove shape retrieval using second function

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -3,20 +3,6 @@
 '''
 
 
-def get_shape(matrix, shape):
-    '''Recoursively finds the shape of a multidimensional matrix
-
-    Args:
-        matrix: multidimensional matrix in need of shape finding
-        shape: empty list to store shape
-    '''
-    if type(matrix[0]) is list:
-        shape.append(len(matrix))
-        get_shape(matrix[0], shape)
-    else:
-        shape.append(len(matrix))
-
-
 def add_matrices2D(mat1, mat2):
     '''Adds two matrices element-wise
 
@@ -28,20 +14,15 @@ def add_matrices2D(mat1, mat2):
     '''
 
     added_mat = []
-    mat1_shape = []
-    mat2_shape = []
-    get_shape(mat1, mat1_shape)
-    get_shape(mat2, mat2_shape)
 
     # Compare matrices shapes if different returns None
-    for el1, el2 in zip(mat1_shape, mat2_shape):
-        if el1 != el2:
-            return None
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
 
     # Adds the two matrices element-wise
-    for row_idx in range(mat1_shape[0]):
+    for row_idx in range(len(mat1)):
         arr = []
-        for col_idx in range(mat1_shape[1]):
+        for col_idx in range(len(mat1[0])):
             arr.append(mat1[row_idx][col_idx] + mat2[row_idx][col_idx])
         added_mat.append(arr)
     return added_mat
