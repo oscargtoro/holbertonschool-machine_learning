8608bc8d33f8bbf0efe8fe37b17bc89b85cd1dc2
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-03 10:05:28 -0500

    Fixed comparison in shape

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -16,6 +16,7 @@ def get_shape(matrix, shape):
     else:
         shape.append(len(matrix))
 
+
 def add_matrices2D(mat1, mat2):
     '''Adds two matrices element-wise
 
@@ -31,12 +32,16 @@ def add_matrices2D(mat1, mat2):
     mat2_shape = []
     get_shape(mat1, mat1_shape)
     get_shape(mat2, mat2_shape)
-    if mat1_shape != mat2_shape:
-        return None
-    else:
-        for row_idx in range(mat1_shape[0]):
-            arr = []
-            for col_idx in range(mat1_shape[1]):
-                arr.append(mat1[row_idx][col_idx] + mat2[row_idx][col_idx])
-            added_mat.append(arr)
-        return added_mat
+
+    # Compare matrices shapes if different returns None
+    for el1, el2 in zip(mat1_shape, mat2_shape):
+        if el1 != el2:
+            return None
+
+    # Adds the two matrices element-wise
+    for row_idx in range(mat1_shape[0]):
+        arr = []
+        for col_idx in range(mat1_shape[1]):
+            arr.append(mat1[row_idx][col_idx] + mat2[row_idx][col_idx])
+        added_mat.append(arr)
+    return added_mat
