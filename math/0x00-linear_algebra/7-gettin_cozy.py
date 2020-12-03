be44fbbab5daa85835e4110213afdd19a4b93d94
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-03 11:47:44 -0500

    Added column and row check depending on the axis

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Concatenates tow matrices by a specific axis
+'''Concatenates two matrices by a specific axis
 '''
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    '''Concatenates tow matrices by a specific axis.
+    '''Concatenates two matrices by a specific axis.
 
     Args:
         mat1, mat2: Matrices to concatenate.
@@ -15,15 +15,17 @@ def cat_matrices2D(mat1, mat2, axis=0):
     '''
 
     cat_matrix = []
-    if len(mat1) != len(mat2):
-        return None
     if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         for el1, el2 in zip(mat1, mat2):
             conc_mat = [x for x in el1]
             conc_mat.extend([x for x in el2])
             cat_matrix.append(conc_mat)
         return cat_matrix
     elif axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         cat_matrix.extend([[x for x in row] for row in mat1])
         cat_matrix.extend([[x for x in row] for row in mat2])
         return cat_matrix
