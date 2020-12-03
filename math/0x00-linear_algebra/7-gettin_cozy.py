0a51455f099c936d7b2c6d7e9372aa49615610a2
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-03 11:37:46 -0500

    Added axis check

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -15,16 +15,15 @@ def cat_matrices2D(mat1, mat2, axis=0):
     '''
 
     cat_matrix = []
-    try:
-        if axis == 1:
-            for el1, el2 in zip(mat1, mat2):
-                conc_mat = [x for x in el1]
-                conc_mat.extend([x for x in el2])
-                cat_matrix.append(conc_mat)
-            return cat_matrix
-        else:
-            cat_matrix.extend([[x for x in row] for row in mat1])
-            cat_matrix.extend([[x for x in row] for row in mat2])
-            return cat_matrix
-    except:
+    if axis == 1:
+        for el1, el2 in zip(mat1, mat2):
+            conc_mat = [x for x in el1]
+            conc_mat.extend([x for x in el2])
+            cat_matrix.append(conc_mat)
+        return cat_matrix
+    elif axis == 0:
+        cat_matrix.extend([[x for x in row] for row in mat1])
+        cat_matrix.extend([[x for x in row] for row in mat2])
+        return cat_matrix
+    else:
         return None
