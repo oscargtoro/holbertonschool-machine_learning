977a42530d52b764be70600beb7dd24752a1dc1c
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-10 08:45:46 -0500

    Added check for 0

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -15,6 +15,6 @@ def summation_i_squared(n):
         the sum of i to the power of 2 from i equals 1 to n
     '''
 
-    if not isinstance(n, int) or n < 0:
+    if not isinstance(n, int) or n <= 0:
         return None
     return int((n*(n + 1)*(2*n + 1)) / 6)
