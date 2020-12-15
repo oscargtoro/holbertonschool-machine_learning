114dd03a4f96e5dd0fa4546a10d2e0be9ee9f71b
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-15 16:45:11 -0500

    Class representing an exponential distribution

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''Exponential class module
+'''
+
+
+class Exponential():
+    '''Represents an exponential distribution
+    '''
+
+    def __init__(self, data=None, lambtha=1.):
+        '''Contstructor for class Exponential.
+
+        Args:
+            data: List of data to be used to estimate the distribution.
+            lambtha: Expected number of occurences in a given time frame.
+        '''
+
+        if data is None:
+            if lambtha < 0:
+                raise ValueError('lambtha must be a positive value')
+            else:
+                self.lambtha = lambtha
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = float(1 / (sum(data) / len(data)))
