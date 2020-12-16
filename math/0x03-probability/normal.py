1c7062fb35fe950c290baae1873a9abf175d1857
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-16 07:34:48 -0500

    Added z-score and x-score methods

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -11,9 +11,9 @@ class Normal():
         '''Normal class constructor.
 
         Args.
-            data: List of the data to be used to estimate the distribution
-            mean: Mean of the distribution
-            stddev: Standard deviation of the distribution
+            data: List of the data to be used to estimate the distribution.
+            mean: Mean of the distribution.
+            stddev: Standard deviation of the distribution.
         '''
 
         if data is None:
@@ -31,3 +31,24 @@ class Normal():
             for point in data:
                 variance += (self.mean - point) ** 2
             self.stddev = float((variance / len(data)) ** 0.5)
+
+    def z_score(self, x):
+        '''Calculates the z-score of a given x-value.
+
+        Args.
+            x: Value used to calculate the z-score.
+
+        Returns.
+            The calculated z-value.
+        '''
+
+        return float((x - self.mean) / self.stddev)
+
+    def x_value(self, z):
+        '''Calculates the x-value of a given z-score.
+
+        Args.
+            z: Value used to calculate the x-score.
+        '''
+
+        return float((self.stddev * z) + self.mean)
