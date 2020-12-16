ba6158de8cc771867b7788c92424e5f291f95815
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-16 13:30:57 -0500

    Binomial class with its constructor method

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''Module for the binomial class.
+'''
+
+
+class Binomial():
+    '''Representation of a binomial distribution.
+    '''
+
+    def __init__(self, data=None, n=1, p=0.5):
+        '''Binomial class constructor.
+
+        Args.
+            data: List of the data to be used to estimate the distribution
+            n: Number of Bernoulli trials
+            p: Probability of a success
+        '''
+
+        if data is None:
+            if n < 0:
+                raise ValueError('n must be a positive value')
+            if 0 <= p >= 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            mean = float(sum(data) / len(data))
+            variance = float(sum([(x - mean) ** 2 for x in data]) / len(data))
+            p = 1 - variance / mean
+            self.n = round(mean / p)
+            self.p = float(mean / self.n)
