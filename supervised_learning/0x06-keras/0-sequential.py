ecbd8be5b385b69c75171154e044d1b63e7a832e
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-01-25 07:40:24 -0500

    Builds a neural network with the Keras library

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''Module for the function
+build_model(nx, layers, activations, lambtha, keep_prob)
+'''
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''Builds a neural network with the Keras library.
+
+    Args.
+        nx: The number of input features to the network.
+        layers: A list containing the number of nodes in each layer of
+        the network.
+        activations: A list containing the activation functions used for
+        each layer of the network.
+        lambtha: The L2 regularization parameter.
+        keep_prob: The probability that a node will be kept for dropout.
+    Returns.
+        A keras model.
+    '''
+
+    model = K.Sequential()
+    l2 = K.regularizers.l2(lambtha)
+
+    for i in range(len(layers) - 1):
+        model.add(K.layers.Dense(units=layers[i],
+                                 activation=activations[0],
+                                 kernel_regularizer=l2,
+                                 input_shape=(nx,)))
+        model.add(K.layers.Dropout(1 - keep_prob, input_shape=(layers[i],)))
+
+    model.add(K.layers.Dense(units=layers[i + 1],
+                             activation=activations[0],
+                             kernel_regularizer=l2,
+                             input_shape=(nx,)))
+    return model
