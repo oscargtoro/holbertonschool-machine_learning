b7c703792fc0d831f4018fc9878fcabba1915803
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-01-26 12:27:48 -0500

    Handles save and load configuration of models

---
+++ supervised_learning/0x06-keras/11-config.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''
+'''
+
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    '''Saves a model’s configuration in JSON format.
+
+    Args:
+        network: The model whose configuration should be saved.
+        filename: The path of the file that the configuration should
+        be saved to
+
+    Returns:
+        None
+    '''
+
+    with open(filename, 'w') as f:
+        f.write(network.to_json())
+
+    return None
+
+
+def load_config(filename):
+    '''Loads a model with a specific configuration.
+
+    Args:
+        filename: The path of the file containing the model’s configuration
+        in JSON format.
+
+    Returns:
+        The loaded model
+    '''
+
+    with open(filename) as f:
+        model = K.models.model_from_json(f.read())
+
+    return model
