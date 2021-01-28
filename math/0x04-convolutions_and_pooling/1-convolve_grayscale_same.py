0d6ce02e39c08f29b93ed9288bd163c77bec72bc
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-01-28 15:52:42 -0500

    Performs a same convolution on grayscale images

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -24,4 +24,39 @@ def convolve_grayscale_same(images, kernel):
     Returns:
     A numpy.ndarray containing the convolved images.
     """
-    pass
+
+    in_d = images.shape[0]
+    in_h = images.shape[1]
+    in_w = images.shape[2]
+    k_h = kernel.shape[0]
+    k_w = kernel.shape[1]
+    out_h = in_h
+    out_w = in_w
+    out_d = in_d
+
+    # Calculate the number of zeros which are needed to add as padding
+    pad_along_height = max((out_h - 1) + k_h - in_h, 0)
+    pad_along_width = max((out_w - 1) + k_w - in_w, 0)
+    # amount of zero padding on the top
+    pad_top = pad_along_height // 2
+    # amount of zero padding on the bottom
+    pad_bottom = pad_along_height - pad_top
+    # amount of zero padding on the left
+    pad_left = pad_along_width // 2
+    # amount of zero padding on the right
+    pad_right = pad_along_width - pad_left
+    output = np.zeros((out_d, out_h, out_w))
+
+    images_padded = np.zeros((in_d,
+                              in_h + pad_along_height,
+                              in_w + pad_along_width))
+    images_padded[:, pad_top:-pad_bottom, pad_left:-pad_right] = images
+
+    for h in range(out_h):
+        for w in range(out_w):
+            output[:, h, w] = (
+                               kernel *
+                               images_padded[:, h:h + k_h, w:w + k_w]
+                               ).sum(axis=(1, 2))
+
+    return output
