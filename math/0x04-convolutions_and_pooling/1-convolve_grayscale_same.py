14cc0939b54219cb7a66de954151ea0ccba8aa58
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-01-28 15:25:39 -0500

    changed single quote for double quote

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
-'''Module for the function
+"""Module for the function
 convolve_grayscale_same(images, kernel).
-'''
+"""
 
 import numpy as np
 from math import ceil, floor
 
 
 def convolve_grayscale_same(images, kernel):
-    '''Performs a same convolution on grayscale images.
-    '''
+    """Performs a same convolution on grayscale images.
+
+    Args:
+        images: numpy.ndarray with shape (m, h, w) containing multiple
+        grayscale images.
+            - m: The number of images.
+            - h: The height in pixels of the images.
+            - w: The width in pixels of the images.
+        kernel: numpy.ndarray with shape (kh, kw) containing the kernel
+        for the convolution.
+            - kh: The height of the kernel.
+            - kw: The width of the kernel.
+
+    Returns:
+    A numpy.ndarray containing the convolved images.
+    """
     pass
