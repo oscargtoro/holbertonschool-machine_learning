a17e4192f7e1a1c1178717dd94564c08d72521f9
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-01 09:45:06 -0500

    Changed code to use np.pad in padded np creation

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -30,31 +30,25 @@ def convolve_grayscale_same(images, kernel):
     k_h = kernel.shape[0]
     k_w = kernel.shape[1]
 
-    # define top, bottom, left and right padding
     if k_h % 2 == 0:
-        pad_h = k_h
+        pad_h = k_h // 2
     else:
-        pad_h = k_h - 1
+        pad_h = (k_h - 1) // 2
     if k_w % 2 == 0:
-        pad_w = k_w
+        pad_w = k_w // 2
     else:
-        pad_w = k_w - 1
-    pad_t = pad_h // 2
-    pad_b = pad_h - pad_t
-    pad_l = pad_w // 2
-    pad_r = pad_w - pad_l
+        pad_w = (k_w - 1) // 2
 
-    # create a new numpy array with the necessary shape for same output
-    images_padded = np.zeros((in_d, in_h + pad_h, in_w + pad_w))
-    images_padded[:, pad_t:-pad_b, pad_l:-pad_r] = images
+    pad_size = ((0, 0), (pad_h, pad_h), (pad_w, pad_w))
+    images_padded = np.pad(images,
+                           pad_width=pad_size,
+                           mode='constant',
+                           constant_values=0)
 
-    # create the output numpy array
-    out_h = in_h + (2 * (pad_t)) - k_h + 1
-    out_w = in_w + (2 * (pad_l)) - k_w + 1
-    output = np.zeros((in_d, out_h, out_w))
+    output = np.zeros((in_d, in_h, in_w))
 
-    for h in range(out_h):
-        for w in range(out_w):
+    for h in range(in_h):
+        for w in range(in_w):
             output[:, h, w] = (
                                kernel *
                                images_padded[:, h:h + k_h, w:w + k_w]
