8329348aec2df12063aa919a87e02d9f85bb6189
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-01 11:35:22 -0500

    removed ceil and floor import

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -4,7 +4,6 @@ convolve_grayscale(images, kernel, padding='same', stride=(1, 1)).
 '''
 
 import numpy as np
-from math import ceil, floor
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
