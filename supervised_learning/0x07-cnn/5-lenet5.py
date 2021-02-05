5fc14923779e78faba76152f582386338d1a85b6
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-04 20:41:24 -0500

    Empty file for task 5 with docstrings and def

---
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''Module for the function
+lenet5(x, y).
+'''
+
+import tensorflow.keras as K
+
+
+def lenet5(x, y):
+    '''Modified version of the LeNet-5 architecture using keras.
+
+    Args:
+        x: keras.Input of shape (m, 28, 28, 1) containing the input images
+        for the network
+            -m: The number of images.
+
+    Returns:
+        a K.Model compiled to use Adam optimization (with default
+        hyperparameters) and accuracy metrics
+    '''
+    pass
