89359e498534795748c5fd2addc7b8781cb5d056
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-11 10:04:01 -0500

    Added import of previous task

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -4,6 +4,7 @@ as described in Going Deeper with Convolutions (2014).
 """
 
 import tensorflow.keras as K
+inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():
