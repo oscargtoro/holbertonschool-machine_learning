e9d4e4d1b7879be24b71674d95701e5d78391d2a
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-11 09:08:01 -0500

    Task 1 initial commit

---
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for the function inception_network that builds the inception network
+as described in Going Deeper with Convolutions (2014).
+"""
+
+import tensorflow.keras as K
+
+
+def inception_network():
+    """Builds the inception network as described in Going Deeper with
+    Convolutions (2014).
+
+    Returns:
+        The keras model of the inception network.
+    """
+    pass
