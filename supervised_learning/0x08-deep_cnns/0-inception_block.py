dfd5d7a17aa0f6df6cd178a862fedd088f634a28
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-11 12:38:49 -0500

    Added code for specific number of filters

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -27,4 +27,52 @@ def inception_block(A_prev, filters):
     Returns:
         The concatenated output of the inception block
     """
-    pass
+
+    F1, F3R, F3, F5R, F5, FPP = filters
+    concatf = []
+
+    # Perform 1x1 convolution
+    concatf.append(K.layers.Conv2D(filters=F1,
+                                   kernel_size=1,
+                                   padding="same",
+                                   activation="relu")(A_prev))
+
+    # 1x1 convolution for next 3x3 convolution
+    # improving performance and accuracy
+    F3R_layer = K.layers.Conv2D(filters=F3R,
+                                kernel_size=1,
+                                padding="same",
+                                activation="relu")(A_prev)
+
+    # 3x3 convolution using output from 1x1 convolution
+    concatf.append(K.layers.Conv2D(filters=F3,
+                                   kernel_size=3,
+                                   padding="same",
+                                   activation="relu")(F3R_layer))
+
+    # 1x1 convolution for next 5x5 convolution
+    # improving performance and accuracy
+    F5R_layer = K.layers.Conv2D(filters=F5R,
+                                kernel_size=1,
+                                padding="same",
+                                activation="relu")(A_prev)
+
+    # 3x3 convolution using output from 1x1 convolution
+    concatf.append(K.layers.Conv2D(filters=F5,
+                                   kernel_size=5,
+                                   padding="same",
+                                   activation="relu")(F5R_layer))
+
+    # Perform max pooling in the previous output
+    maxp_layer = K.layers.MaxPool2D(pool_size=3,
+                                    strides=1,
+                                    padding="same")(A_prev)
+
+    # 1x1 convolution for next 5x5 convolution
+    # improving performance and accuracy
+    concatf.append(K.layers.Conv2D(filters=FPP,
+                                   kernel_size=1,
+                                   padding="same",
+                                   activation="relu")(maxp_layer))
+
+    return K.layers.Concatenate(axis=-1)(concatf)
