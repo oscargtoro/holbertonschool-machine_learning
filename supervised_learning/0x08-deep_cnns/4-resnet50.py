84124f5178083314112dc3781fd1ef0defbea4b7
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-15 15:26:43 -0500

    Builds the ResNet-50 architecture as described in Deep Residual     Learning for Image Recognition (2015).

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -16,4 +16,37 @@ def resnet50():
     Returns:
         The keras model
     """
-    pass
+
+    init = K.initializers.he_normal()
+    X = K.layers.Input(shape=(224, 224, 3))
+
+    Y = K.layers.Conv2D(filters=64,
+                        kernel_size=7,
+                        strides=2,
+                        padding="same",
+                        kernel_initializer=init)(X)
+    Y = K.layers.BatchNormalization()(Y)
+    Y = K.layers.Activation("relu")(Y)
+    Y = K.layers.MaxPool2D(pool_size=3, strides=2, padding="same")(Y)
+    Y = projection_block(A_prev=Y, filters=(64, 64, 256), s=1)
+    Y = identity_block(A_prev=Y, filters=(64, 64, 256))
+    Y = identity_block(A_prev=Y, filters=(64, 64, 256))
+    Y = projection_block(A_prev=Y, filters=(128, 128, 512), s=2)
+    Y = identity_block(A_prev=Y, filters=(128, 128, 512))
+    Y = identity_block(A_prev=Y, filters=(128, 128, 512))
+    Y = identity_block(A_prev=Y, filters=(128, 128, 512))
+    Y = projection_block(A_prev=Y, filters=(256, 256, 1024), s=2)
+    Y = identity_block(A_prev=Y, filters=(256, 256, 1024))
+    Y = identity_block(A_prev=Y, filters=(256, 256, 1024))
+    Y = identity_block(A_prev=Y, filters=(256, 256, 1024))
+    Y = identity_block(A_prev=Y, filters=(256, 256, 1024))
+    Y = identity_block(A_prev=Y, filters=(256, 256, 1024))
+    Y = projection_block(A_prev=Y, filters=(512, 512, 2048), s=2)
+    Y = identity_block(A_prev=Y, filters=(512, 512, 2048))
+    Y = identity_block(A_prev=Y, filters=(512, 512, 2048))
+    Y = K.layers.AveragePooling2D(pool_size=7, padding="same")(Y)
+    Y = K.layers.Dense(units=1000,
+                       activation="relu",
+                       kernel_initializer=init)(Y)
+    model = K.models.Model(X, Y)
+    return model
