1c7342a23911e70b73296f147c8cec798a4c6096
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-15 17:39:19 -0500

    Builds a dense block as described in Densely Connected Convolutional     Networks.

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -21,4 +21,31 @@ def dense_block(X, nb_filters, growth_rate, layers):
         The concatenated output of each layer within the Dense Block and the
         number of filters within the concatenated outputs, respectively.
     """
-    pass
+
+    init = K.initializers.he_normal()
+    output = [X]
+    # don't understand why, just that the output expected is 4 times the
+    # growth rate
+    c11f = growth_rate * 4
+
+    # for each layer in layers we use bottleneck layers (1x1 layers) before
+    # the 3x3 conv
+    for layer in range(layers):
+        X_prev = output[layer]
+        H = K.layers.BatchNormalization()(X_prev)
+        H = K.layers.Activation(activation="relu")(H)
+        # bottleneck layer
+        X = K.layers.Conv2D(filters=c11f,
+                            kernel_size=1,
+                            padding="same",
+                            kernel_initializer=init)(H)
+        H = K.layers.BatchNormalization()(X)
+        H = K.layers.Activation(activation="relu")(H)
+        X = K.layers.Conv2D(filters=growth_rate,
+                            kernel_size=3,
+                            padding="same",
+                            kernel_initializer=init)(H)
+        nb_filters += growth_rate
+        output.append(K.layers.Concatenate()([output[layer], X]))
+
+    return output, nb_filters
