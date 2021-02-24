8dc784da3ab4c68ac9a68cd77537119ccea09c3e
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-24 17:20:33 -0500

    formulas

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -38,28 +38,33 @@ class Yolo:
         boxes = []
         box_confidences = []
         box_class_probs = []
-        for output in outputs:
-            boxes.append(output[:, :, :, :4])
-            box_confidences.append(output[:, :, :, 4:5])
-            # print(output[:, :, :, 4:5])
-            box_class_probs.append(output[:, :, :, 5:])
-            # (grid_h, grid_w, anchors, attributes) = output.shape
-            # confidence = np.zeros((grid_h, grid_w, anchors, 1))
-            # box = np.zeros((grid_h, grid_w, anchors, 4))
-            # classes = np.zeros((grid_h, grid_w, anchors, attributes - 5))
-            # for h in range(grid_h):
-            #     for w in range(grid_w):
-            #         x1 = y1 = x2 = y2 = 0
-            #         confidences = []
-            #         for a in range(anchors):
-            #             # x1 = output[h, w, a, 0] - (grid_w / 2)
-            #             # y1 = output[h, w, a, 1] + (grid_h / 2)
-            #             # x2 = output[h, w, a, 0] + (grid_w / 2)
-            #             # y2 = output[h, w, a, 1] - (grid_h / 2)
-            #             box[h, w, a, :] = output[h, w, a, :4]
-            #             confidence[h, w, a, :] = output[h, w, a, 4]
-            #             classes[h, w, a, :] = output[h, w, a, 5:]
-            # boxes.append(box)
-            # box_confidences.append(confidence)
-            # box_class_probs.append(classes)
+        height, width = image_size
+
+        for idx, output in enumerate(outputs):
+            (grid_h, grid_w, in_archons, _) = output.shape
+            box = output[:, :, :, :4]
+            for h in range(grid_h):
+                for w in range(grid_w):
+                    for a in range(in_archons):
+                        pw, ph = self.anchors[idx][a]
+                        tx, ty, tw, th = output[h, w, a, 0:4]
+                        bx = (1 / (1 + np.exp(tx))) + w
+                        bx = bx / grid_w
+                        by = (1 / (1 + np.exp(ty))) + h
+                        by = by / grid_h
+                        bw = pw * np.exp(tw)
+                        bw = bw / self.model.input.shape[1].value
+                        bh = ph * np.exp(th)
+                        bh = bh / self.model.input.shape[2].value
+                        x1 = (bx - bw / 2)
+                        x2 = (x1 + bw)
+                        y1 = (by - bh / 2)
+                        y2 = (y1 + bh)
+                        box[h, w, a, 0] = x1 * width
+                        box[h, w, a, 1] = y1 * height
+                        box[h, w, a, 2] = x2 * width
+                        box[h, w, a, 3] = y2 * height
+            boxes.append(box)
+            box_confidences.append(1 / (1 + np.exp(output[:, :, :, 4:5])))
+            box_class_probs.append(1 / (1 + np.exp(output[:, :, :, 5:])))
         return boxes, box_confidences, box_class_probs
