01a6c2d8db8c373ea45d3b7f2d6593bba61c2ccb
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-24 14:45:25 -0500

    Added the process_outputs method

---
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Module for the class YOLO
+"""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """Uses Yolo V3 algorithm to perform object detection
+    """
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Initializer for the Yolo class
+        """
+
+        self.model = K.models.load_model(model_path)
+        self.class_names = []
+        with open(classes_path, "r") as classes:
+            for line in classes:
+                self.class_names.append(line[:-1])
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def process_outputs(self, outputs, image_size):
+        """
+        Args:
+            outputs: list of numpy.ndarrays containing the predictions from
+            the Darknet model for a single image.
+            image_size: numpy.ndarray containing the image’s original
+            size [image_height, image_width]
+
+        Returns:
+            A tuple of (boxes, box_confidences, box_class_probs)
+        """
+
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+        for output in outputs:
+            (grid_h, grid_w, anchors, attributes) = output.shape
+            confidence = np.zeros((grid_h, grid_w, anchors, 1))
+            box = np.zeros((grid_h, grid_w, anchors, 4))
+            classes = np.zeros((grid_h, grid_w, anchors, attributes - 5))
+            for h in range(grid_h):
+
+                i = 0
+                for w in range(grid_w):
+                    x1 = y1 = x2 = y2 = 0
+                    confidences = []
+                    for a in range(anchors):
+                        x1 = output[h, w, a, 0] - (output[h, w, a, 2] / 2)
+                        y1 = output[h, w, a, 1] + (output[h, w, a, 3] / 2)
+                        x2 = output[h, w, a, 0] + (output[h, w, a, 2] / 2)
+                        y2 = output[h, w, a, 1] - (output[h, w, a, 3] / 2)
+                        box[h, w, a, :] = [x1, y1, x2, y2]
+                        confidence[h, w, a, :] = output[h, w, a, 4]
+                        classes[h, w, a, :] = output[h, w, a, 5:]
+            boxes.append(box)
+            box_confidences.append(confidence)
+            box_class_probs.append(classes)
+        return boxes, box_confidences, box_class_probs
