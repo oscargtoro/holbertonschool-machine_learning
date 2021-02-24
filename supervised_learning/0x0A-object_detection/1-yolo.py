64b757cd6eb7336c58310d4bed0df98e1f30f615
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-02-24 15:03:09 -0500

    Testing requirements

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -3,7 +3,6 @@
 """
 
 import tensorflow.keras as K
-import numpy as np
 
 
 class Yolo:
@@ -38,26 +37,24 @@ class Yolo:
         boxes = []
         box_confidences = []
         box_class_probs = []
-        for output in outputs:
-            (grid_h, grid_w, anchors, attributes) = output.shape
-            confidence = np.zeros((grid_h, grid_w, anchors, 1))
-            box = np.zeros((grid_h, grid_w, anchors, 4))
-            classes = np.zeros((grid_h, grid_w, anchors, attributes - 5))
-            for h in range(grid_h):
-
-                i = 0
-                for w in range(grid_w):
-                    x1 = y1 = x2 = y2 = 0
-                    confidences = []
-                    for a in range(anchors):
-                        x1 = output[h, w, a, 0] - (output[h, w, a, 2] / 2)
-                        y1 = output[h, w, a, 1] + (output[h, w, a, 3] / 2)
-                        x2 = output[h, w, a, 0] + (output[h, w, a, 2] / 2)
-                        y2 = output[h, w, a, 1] - (output[h, w, a, 3] / 2)
-                        box[h, w, a, :] = [x1, y1, x2, y2]
-                        confidence[h, w, a, :] = output[h, w, a, 4]
-                        classes[h, w, a, :] = output[h, w, a, 5:]
-            boxes.append(box)
-            box_confidences.append(confidence)
-            box_class_probs.append(classes)
+        # for output in outputs:
+        #     (grid_h, grid_w, anchors, attributes) = output.shape
+        #     confidence = np.zeros((grid_h, grid_w, anchors, 1))
+        #     box = np.zeros((grid_h, grid_w, anchors, 4))
+        #     classes = np.zeros((grid_h, grid_w, anchors, attributes - 5))
+        #     for h in range(grid_h):
+        #         for w in range(grid_w):
+        #             x1 = y1 = x2 = y2 = 0
+        #             confidences = []
+        #             for a in range(anchors):
+        #                 x1 = output[h, w, a, 0] - (output[h, w, a, 2] / 2)
+        #                 y1 = output[h, w, a, 1] + (output[h, w, a, 3] / 2)
+        #                 x2 = output[h, w, a, 0] + (output[h, w, a, 2] / 2)
+        #                 y2 = output[h, w, a, 1] - (output[h, w, a, 3] / 2)
+        #                 box[h, w, a, :] = [x1, y1, x2, y2]
+        #                 confidence[h, w, a, :] = output[h, w, a, 4]
+        #                 classes[h, w, a, :] = output[h, w, a, 5:]
+        #     boxes.append(box)
+        #     box_confidences.append(confidence)
+        #     box_class_probs.append(classes)
         return boxes, box_confidences, box_class_probs
