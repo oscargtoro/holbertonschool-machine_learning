da6a7cc7160fcb2139926c4c264f904ffb64ceee
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-03-01 10:10:55 -0500

    Use prev layer to create new layer on top Added args to docstring

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -7,7 +7,16 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation):
     '''Createa a tensor layer
+
+    Args:
+        prev: The tensor output of the previous layer
+        n: The number of nodes in the layer to create
+        activation: The activation function that the layer should use
     '''
 
-    model = tf.layers.Dense(units=n, activation=activation, name='layer')
-    return model(prev)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    x = tf.layers.Dense(units=n,
+                        activation=activation,
+                        name='layer',
+                        kernel_initializer=init)(prev)
+    return x
