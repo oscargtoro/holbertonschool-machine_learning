81512b6a58eb0bedf3d6309b0ac91229459f4949
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-03-04 11:07:34 -0500

    Complete code

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-'''Module for the function
-lenet5(x, y).
+'''Module for the function lenet5.
+Builds a modified version of the LeNet-5 architecture using tensorflow
 '''
 
 import tensorflow as tf
 
 
 def lenet5(x, y):
-    '''Modified version of the LeNet-5 architecture using tensorflow.
+    '''Builds a modified version of the LeNet-5 architecture using tensorflow.
 
     Args:
         x: tf.placeholder of shape (m, 28, 28, 1) containing the input images
@@ -21,4 +21,41 @@ def lenet5(x, y):
     utilizes Adam optimization (with default hyperparameters), a tensor
     for the loss of the netowrk, a tensor for the accuracy of the network.
     '''
-    pass
+
+    init = tf.contrib.layers.variance_scaling_initializer()
+    relu = tf.nn.relu
+
+    x = tf.layers.Conv2D(filters=6,
+                         kernel_size=(5, 5),
+                         padding='same',
+                         activation=relu,
+                         kernel_initializer=init)(x)
+
+    x = tf.layers.MaxPooling2D(pool_size=2, strides=2)(x)
+
+    x = tf.layers.Conv2D(filters=16,
+                         kernel_size=(5, 5),
+                         padding='valid',
+                         activation=relu,
+                         kernel_initializer=init)(x)
+
+    x = tf.layers.MaxPooling2D(pool_size=2, strides=2)(x)
+
+    x = tf.layers.Flatten()(x)
+
+    x = tf.layers.Dense(units=120, activation=relu, kernel_initializer=init)(x)
+
+    x = tf.layers.Dense(units=84, activation=relu, kernel_initializer=init)(x)
+
+    output = tf.layers.Dense(units=10, kernel_initializer=init)(x)
+
+    y_pred = tf.nn.softmax(output)
+
+    loss = tf.losses.softmax_cross_entropy(y, logits=output)
+
+    train_op = tf.train.AdamOptimizer().minimize(loss)
+
+    equal = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
+    acc = tf.reduce_mean(tf.cast(equal, tf.float32))
+
+    return y_pred, train_op, loss, acc
