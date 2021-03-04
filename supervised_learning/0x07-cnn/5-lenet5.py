f0c37599d899808a45011fb9860835ad51593556
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-03-04 11:14:34 -0500

    Builds a modified LeNet-5 using keras

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-'''Module for the function
-lenet5(x, y).
+'''Module for the function lenet5.
+Builds a modified version of the LeNet-5 architecture using keras.
 '''
 
 import tensorflow.keras as K
 
 
-def lenet5(x, y):
-    '''Modified version of the LeNet-5 architecture using keras.
+def lenet5(X):
+    '''Builds a modified version of the LeNet-5 architecture using keras.
 
     Args:
         x: keras.Input of shape (m, 28, 28, 1) containing the input images
@@ -15,7 +15,47 @@ def lenet5(x, y):
             -m: The number of images.
 
     Returns:
-        a K.Model compiled to use Adam optimization (with default
+        A keras Model compiled to use Adam optimization (with default
         hyperparameters) and accuracy metrics
     '''
-    pass
+    init = K.initializers.he_normal(seed=None)
+
+    x = K.layers.Conv2D(filters=6,
+                        kernel_size=5,
+                        padding='same',
+                        activation='relu',
+                        kernel_initializer=init)(X)
+
+    x = K.layers.MaxPool2D(pool_size=2, strides=(2, 2))(x)
+
+    x = K.layers.Conv2D(filters=16,
+                        kernel_size=5,
+                        padding='valid',
+                        activation='relu',
+                        kernel_initializer=init)(x)
+
+    x = K.layers.MaxPool2D(pool_size=2, strides=(2, 2))(x)
+
+    x = K.layers.Flatten()(x)
+
+    x = K.layers.Dense(units=120,
+                       activation='relu',
+                       kernel_initializer=init)(x)
+
+    x = K.layers.Dense(units=84,
+                       activation='relu',
+                       kernel_initializer=init)(x)
+
+    output = K.layers.Dense(units=10,
+                            activation='softmax',
+                            kernel_initializer=init)(x)
+
+    model = K.Model(inputs=X, outputs=output)
+
+    Adam = K.optimizers.Adam()
+
+    model.compile(optimizer=Adam,
+                  loss='categorical_crossentropy',
+                  metrics=['acc'])
+
+    return model
