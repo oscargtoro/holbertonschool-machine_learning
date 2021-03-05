Fixed broadcast error

supervised_learning/0x01-classification/18-deep_neural_network.py
@@ -68,8 +68,8 @@ class DeepNeuralNetwork:
         self.__cache["A0"] = X
         for i in range(self.__L):
             cache = self.__cache["A{}".format(i)]
-            b_weights = self.__weights["b{}".format(i+1)]
-            w_weights = self.__weights["W{}".format(i+1)]
-            Z = np.matmul(w_weights, cache + b_weights)
+            b_weight = self.__weights["b{}".format(i + 1)]
+            w_weight = self.__weights["W{}".format(i + 1)]
+            Z = np.matmul(w_weight, cache) + b_weight
             self.__cache["A{}".format(i + 1)] = (np.exp(Z) / (np.exp(Z) + 1))
         return (self.__cache["A{}".format(i + 1)], self.__cache)

supervised_learning/0x01-classification/26-deep_neural_network.py
@@ -72,7 +72,7 @@ class DeepNeuralNetwork:
             cache = self.__cache["A{}".format(i)]
             b_weights = self.__weights["b{}".format(i+1)]
             w_weights = self.__weights["W{}".format(i+1)]
-            Z = np.matmul(w_weights, cache + b_weights)
+            Z = np.matmul(w_weights, cache) + b_weights
             self.__cache["A{}".format(i + 1)] = (np.exp(Z) / (np.exp(Z) + 1))
         return (self.__cache["A{}".format(i + 1)], self.__cache)
 