3ceb16dcf442b387b43897c975f901a57e51534e
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-03-23 15:56:01 -0500

    wrong len check

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -19,7 +19,7 @@ def correlation(C):
 
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if len(C) != 2 or C.shape[0] != C.shape[1]:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
     diag = np.sqrt(np.diag(C))
