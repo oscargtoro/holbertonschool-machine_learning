79b609705e939c6c72f4fd80c85d7fcc870c31ad
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-03-31 10:11:41 -0500

    Empty file for task 0

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module for the function initialize
+"""
+
+import numpy as np
+
+
+def initialize(X, k):
+    """Initializes cluster centroids for K-means
+
+    Args.
+        X: is a numpy.ndarray of shape (n, d) containing the dataset that will
+        be used for K-means clustering
+            -n: is the number of data points
+            -d: is the number of dimensions for each data point
+        k: is a positive integer containing the number of clusters
+    Returns:
+        A numpy.ndarray of shape (k, d) containing the initialized centroids
+        for each cluster, or None on failure
+    """
+    pass
