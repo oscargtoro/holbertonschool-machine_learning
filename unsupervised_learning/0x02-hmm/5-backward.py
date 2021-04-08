2c747dac0040090c86a0517823d8124a2d200ab9
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-04-08 13:29:54 -0500

    Empty task file

---
+++ unsupervised_learning/0x02-hmm/5-backward.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module for the function backward
+"""
+
+import numpy as np
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """Performs the backward algorithm for a hidden markov model
+
+    Args.
+        Observation: is a numpy.ndarray of shape (T,) that contains the index
+        of the observation
+            -T is the number of observations
+        Emission: is a numpy.ndarray of shape (N, M) containing the emission
+        probability of a specific observation given a hidden state
+            -Emission[i, j] is the probability of observing j given the hidden
+            state i
+            -N is the number of hidden states
+            -M is the number of all possible observations
+        Transition: is a 2D numpy.ndarray of shape (N, N) containing the
+        transition probabilities
+            -Transition[i, j] is the probability of transitioning from the
+             hidden state i to j
+        Initial: a numpy.ndarray of shape (N, 1) containing the probability of
+        starting in a particular hidden state
+
+    Returns: P, B, or None, None on failure
+        The likelihood of the observations given the model and a numpy.ndarray
+        of shape (N, T) containing the backward path probabilities, or None,
+        None on failure
+    """
+    return None, None
