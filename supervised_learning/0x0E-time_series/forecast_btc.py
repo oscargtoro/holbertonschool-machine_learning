d81a66b410e142f17ca8f8825c8667c3dc41658f
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-05-06 11:16:37 -0500

    Empty task files

---
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""Module for a script that predicts bitcoin value
+"""
---
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""Module for a script that preprocess data for the script forecast_btc.py
+"""
