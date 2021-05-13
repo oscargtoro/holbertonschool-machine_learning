5676d4d1921d673531f3076f284ad3ce3c904e4b
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-05-13 15:08:49 -0500

    Empty task file

---
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module for the method cumulative_bleu
+"""
+
+
+def cumulative_bleu(references, sentence, n):
+    """Calculates the cumulative n-gram BLEU score for a sentence
+
+    Args.
+        references: list of reference translations, each reference translation
+            is a list of the words in the translation
+        sentence: list containing the model proposed sentence
+        n: size of the n-gram to use for evaluation
+
+    Returns.
+        The cumulative n-gram BLEU score
+    """
+    pass
