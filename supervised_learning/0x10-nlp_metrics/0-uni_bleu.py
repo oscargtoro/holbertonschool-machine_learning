a617aca5f5afc24cb675e582c073dc3c700ae68d
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-05-13 12:07:31 -0500

    removed all mention of ntlk and its modules

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -14,23 +14,13 @@ def uni_bleu(references, sentence):
     Returns.
         The unigram BLEU score
     """
-    # sentence = "It is to insure the troops forever hearing the activity guidebook that party direct".split()
-    # ref1 = "It is a guide to action that ensures that the military will forever heed Party commands".split()
-    # ref2 = "It is the guiding principle which guarantees the military forces always being under the command of the Party".split()
-    # ref3 = "It is the practical guide for the army always to heed the directions of the party".split()
-    # references = [ref1, ref2, ref3]
-    # sentence = "the the the the the the the".split()
-    # ref1 = "The cat is on the mat".split()
-    # ref2 = "There is a cat on the mat".split()
-    # references = [ref1, ref2]
     l_ref = []
     tested = set()
     cclip = 0
     for ref in references:
         l_ref.append([word.lower() for word in ref])
     sentence = [word.lower() for word in sentence]
-    # print(float(modified_precision(l_ref, sentence, 1)))
-    # print(sentence_bleu(l_ref, sentence, weights=(1, 0, 0, 0)))
+
     for word in sentence:
         word = word.lower()
         if word in tested:
