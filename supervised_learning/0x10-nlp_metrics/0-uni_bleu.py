71cde68baae0c9b39dca9c81638ae3953f8f776d
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-05-13 12:05:39 -0500

    This code replicates modified_precision, still not the same output as sentence_bleu

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for the method uni_bleu
 """
-from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
+# from nltk.translate.bleu_score import modified_precision, sentence_bleu
 
 
 def uni_bleu(references, sentence):
@@ -15,5 +15,31 @@ def uni_bleu(references, sentence):
     Returns.
         The unigram BLEU score
     """
-    return sentence_bleu(references, sentence, weights=(1, 0, 0, 0))
-    # return 0.0
+    # sentence = "It is to insure the troops forever hearing the activity guidebook that party direct".split()
+    # ref1 = "It is a guide to action that ensures that the military will forever heed Party commands".split()
+    # ref2 = "It is the guiding principle which guarantees the military forces always being under the command of the Party".split()
+    # ref3 = "It is the practical guide for the army always to heed the directions of the party".split()
+    # references = [ref1, ref2, ref3]
+    # sentence = "the the the the the the the".split()
+    # ref1 = "The cat is on the mat".split()
+    # ref2 = "There is a cat on the mat".split()
+    # references = [ref1, ref2]
+    l_ref = []
+    tested = set()
+    cclip = 0
+    for ref in references:
+        l_ref.append([word.lower() for word in ref])
+    sentence = [word.lower() for word in sentence]
+    # print(float(modified_precision(l_ref, sentence, 1)))
+    # print(sentence_bleu(l_ref, sentence, weights=(1, 0, 0, 0)))
+    for word in sentence:
+        word = word.lower()
+        if word in tested:
+            continue
+        ref_count = []
+        for ref in l_ref:
+            ref_count.append(ref.count(word))
+        cclip += min(sentence.count(word), max(ref_count))
+        tested.add(word)
+
+    return cclip / len(sentence)
