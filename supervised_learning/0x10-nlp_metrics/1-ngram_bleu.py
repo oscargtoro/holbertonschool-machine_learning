174d51d4fb8ca48f165e9ed1128324f0592df9d0
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-05-13 14:23:05 -0500

    Added attempt to calculate BLEU score

--- supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for the method ngram_bleu
 """
-# from nltk.translate.bleu_score import modified_precision
+import numpy as np
 
 
 def ngram_bleu(references, sentence, n):
@@ -16,5 +16,38 @@ def ngram_bleu(references, sentence, n):
     Returns.
         The n-gram BLEU score
     """
-    # return modified_precision(references, sentence, n)
-    pass
+
+    l_ref = []
+    candidate = []
+    tested = set()
+    cclip = 0
+    r = []
+    for ref in references:
+        n_ref = []
+        for i in range(0, len(ref), n - 1):
+            if i + n == len(ref) + 1:
+                break
+            n_ref.append(" ".join(ref[i: i + n]).lower())
+
+        r.append(len(n_ref))
+        l_ref.append(n_ref)
+    for i in range(0, len(sentence), n - 1):
+        if i + n == len(sentence) + 1:
+            break
+        candidate.append(" ".join(sentence[i: i + n]).lower())
+
+    c = len(candidate)
+    for n_gram in candidate:
+        if n_gram in tested:
+            continue
+        ref_count = []
+        for ref in l_ref:
+            ref_count.append(ref.count(n_gram))
+        cclip += min(candidate.count(n_gram), max(ref_count))
+        tested.add(n_gram)
+
+    r = min(r, key=lambda x: abs(x - c))
+    if c < r:
+        return (cclip / c) * np.exp(1 - r / c)
+    else:
+        return cclip / c
