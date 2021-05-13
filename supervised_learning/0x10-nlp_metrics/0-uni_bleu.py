9a88f44c422f125ed435385955fc4cdafb425444
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-05-13 10:20:23 -0500

    Different result from "correct" output expected even with lib build to give bleu score

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module for the method uni_bleu
+"""
+# from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
+
+
+def uni_bleu(references, sentence):
+    """Calculates the unigram BLEU score for a sentence
+
+    Args.
+        references: list of reference translations, each reference translation
+            is a list of the words in the translation
+        sentence: list containing the model proposed sentence
+
+    Returns.
+        The unigram BLEU score
+    """
+    # return sentence_bleu(references, sentence)
+    return 0.0
