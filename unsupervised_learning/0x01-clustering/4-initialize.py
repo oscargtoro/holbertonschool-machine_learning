4d9f7d1659747cdc69e4624d8162aa86a4dd7b82
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-08-09 14:07:15 -0500

    Initializes variables for a Gaussian Mixture Model

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -21,4 +21,15 @@ def initialize(X, k):
         for each cluster (initialized as identity matrices),
         or None, None, None on failure
     """
-    return None, None, None
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None, None
+
+    d = X.shape[1]
+    m, _ = kmeans(X, k)
+    pi = np.full((k, ), 1 / k)
+    S = np.full((k, d, d), np.eye(d))
+
+    return pi, m, S
