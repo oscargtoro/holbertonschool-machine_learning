ba46a13a97476329eddc5dec2681d9159a7dc425
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-08-11 13:14:02 -0500

    Calculates the pdf of a Gaussian distribution

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -20,4 +20,27 @@ def pdf(X, m, S):
         A numpy.ndarray of shape (n,) containing the PDF values for each data
         point or None on failure
     """
-    return None
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None
+    if not isinstance(X, np.ndarray) or len(S.shape) != 2:
+        return None
+
+    d = X.shape[1]
+
+    if (d,) != m.shape:
+        return None
+    if (d, d) != S.shape:
+        return None
+
+    S_det = np.linalg.det(S)
+    S_inv = np.linalg.inv(S)
+    divisor = np.sqrt(((2 * np.pi) ** d) * S_det)
+
+    exponent = (-0.5 * np.sum(np.matmul(S_inv,
+                (X.T - m[:, np.newaxis])) *
+                (X.T - m[:, np.newaxis]), axis=0))
+
+    return np.maximum(np.exp(exponent) / divisor, 1e-300)
