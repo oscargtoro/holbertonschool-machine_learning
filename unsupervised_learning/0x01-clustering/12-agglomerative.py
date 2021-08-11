f20f74c5d2b2a956c7ce6cee0dfc146997cf04e0
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-08-11 17:09:00 -0500

    Performs agglomerative clustering on a dataset

--- unsupervised_learning/0x01-clustering/12-agglomerative.py
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -10,11 +10,17 @@ def agglomerative(X, dist):
     """Performs agglomerative clustering on a dataset
 
     Args.
-        X is a numpy.ndarray of shape (n, d) containing the dataset
-        dist is the maximum cophenetic distance for all clusters
+        X: is a numpy.ndarray of shape (n, d) containing the dataset
+        dist: is the maximum cophenetic distance for all clusters
 
     Returns.
         A numpy.ndarray of shape (n,) containing the cluster indices for each
         data point.
     """
-    pass
+
+    Z = scipy.cluster.hierarchy.linkage(X, 'ward')
+    clss = scipy.cluster.hierarchy.fcluster(Z, dist, criterion="distance")
+    scipy.cluster.hierarchy.dendrogram(Z, color_threshold=dist)
+    plt.show()
+
+    return clss
