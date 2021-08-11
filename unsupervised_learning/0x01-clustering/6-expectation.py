695d6d4700925994f6eaf1ff9da0ba1a30470c1b
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-08-11 13:49:44 -0500

    Calculates the E-step in the EM algorithm for a GMM

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -23,4 +23,30 @@ def expectation(X, pi, m, S):
         for each data point in each cluster and the total log likelihood, or
         None, None on failure
     """
-    return None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+
+    n, d = X.shape
+    k = pi.shape[0]
+
+    if (k, d) != m.shape:
+        return None, None
+    if (k, d, d) != S.shape:
+        return None, None
+
+    tmp = np.zeros((k, n))
+
+    for i in range(k):
+        P = pdf(X, m[i], S[i])
+        tmp[i, :] = P * pi[i]
+
+    posProbabilities = tmp / np.sum(tmp, axis=0)
+    logLikelihood = np.sum(np.log(np.sum(tmp, axis=0)))
+
+    return posProbabilities, logLikelihood
