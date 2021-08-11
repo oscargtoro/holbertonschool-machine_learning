222aa9fa9fd99d8243fe5e51d876aa025fe7cf94
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-08-11 15:23:51 -0500

    Finds the best number of clusters for a GMM Using the Bayesian Information Criterion

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -3,7 +3,7 @@
 """
 
 import numpy as np
-expectation_maximization = __import__('7-EM').expectation_maximization
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
@@ -16,8 +16,6 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         to check for (inclusive)
         kmax: is a positive integer containing the maximum number of clusters
         to check for (inclusive)
-            -If kmax is None, kmax should be set to the maximum number of
-            clusters possible
         iterations: is a positive integer containing the maximum number of
         iterations for the EM algorithm
         tol: is a non-negative float containing the tolerance for the EM
@@ -26,6 +24,53 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         information to the standard output
 
     Returns.
-        best_k, best_result, l, b or None, None, None, None on failure
+        The best value for k based on its BIC, a tuple containing:
+            - a numpy.ndarray of shape (k,) containing the cluster priors for
+            the best number of clusters
+            - a numpy.ndarray of shape (k, d) containing the centroid means for
+            the best number of clusters
+            - a numpy.ndarray of shape (k, d, d) containing the covariance
+            matrices for the best number of clusters
+        A numpy.ndarray of shape (kmax - kmin + 1) containing the log
+        likelihood for each cluster size tested, a numpy.ndarray of shape
+        (kmax - kmin + 1) containing the BIC value for each cluster size tested
+        or None, None, None, None on failure
     """
-    return None, None, None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None, None, None
+    if isinstance(kmax, type(None)):
+        kmax = X.shape[0]
+    elif not isinstance(kmax, int) or kmax <= 0:
+        return None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None
+
+    n, d = X.shape
+    best_k = []
+    best_result = []
+    logLikelihood = []
+    b = []
+
+    for k in range(kmin, kmax + 1):
+        best_k.append(k)
+        pi, m, S, _, kloglikelihood = expectation_maximization(X,
+                                                               k,
+                                                               iterations,
+                                                               tol,
+                                                               verbose)
+        best_result.append((pi, m, S))
+        logLikelihood.append(kloglikelihood)
+        p = (k * d * (d + 1) / 2) + (d * k) + k - 1
+        BIC = p * np.log(n) - 2 * kloglikelihood
+        b.append(BIC)
+
+    logLikelihood = np.array(logLikelihood)
+    index = np.argmin(b)
+
+    return best_k[index], best_result[index], logLikelihood, np.array(b)
