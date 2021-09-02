419dc8253414286ebc3e5347e7dc0373e7398f88
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-02 16:36:25 -0500

    class that represents a gated recurrent unit

--- supervised_learning/0x0D-RNNs/2-gru_cell.py
+++ supervised_learning/0x0D-RNNs/2-gru_cell.py
@@ -17,7 +17,15 @@ class GRUCell:
             h: dimensionality of the hidden state
             o: dimensionality of the outputs
         """
-        pass
+
+        self.Wz = np.random.randn(i + h, h)
+        self.Wr = np.random.randn(i + h, h)
+        self.Wh = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
+        self.bz = np.zeros((1, h))
+        self.br = np.zeros((1, h))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
         """Performs forward propagation for one time step
@@ -32,4 +40,14 @@ class GRUCell:
         Returns:
             The next hidden state and the output of the cell
         """
-        pass
+
+        con = np.concatenate((h_prev, x_t), axis=1)
+        z_t = 1 / (1 + np.exp(np.matmul(con, self.Wz) + self.bz))
+        r_t = 1 / (1 + np.exp(np.matmul(con, self.Wr) + self.br))
+        imd = np.concatenate((r_t * h_prev, x_t), axis=1)
+        h_t = np.tanh(np.matmul(imd, self.Wh) + self.bh)
+        h_next = (1 - z_t) * h_prev + z_t *h_t
+        sof_con = np.matmul(h_next, self.Wy) + self.by
+        y = np.exp(sof_con) / np.sum(np.exp(sof_con), axis=1, keepdims=True)
+
+        return h_next, y
