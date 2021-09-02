67cf65ce6a51516a451834d6c437c393e0e6cf0f
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-02 18:31:35 -0500

    Added method backward() to class BidirectionalCell Calculates the hidden state in the backward direction for one time step

--- supervised_learning/0x0D-RNNs/6-bi_backward.py
+++ supervised_learning/0x0D-RNNs/6-bi_backward.py
@@ -17,7 +17,13 @@ class BidirectionalCell:
             h: dimensionality of the hidden states
             o: dimensionality of the outputs
         """
-        pass
+
+        self.Whf = np.random.randn(i + h, h)
+        self.Whb = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h + h, o)
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
         """Calculates the hidden state in the forward direction for one time
@@ -29,11 +35,13 @@ class BidirectionalCell:
                 -m is the batch size for the data
             h_prev: numpy.ndarray of shape (m, h) containing the previous
             hidden state
-
-        Returns.
+        Returns:
             The next hidden state
         """
-        pass
+
+        con = np.concatenate((h_prev, x_t), axis=1)
+
+        return np.tanh(np.matmul(con, self.Whf) + self.bhf)
 
     def backward(self, h_next, x_t):
         """Calculates the hidden state in the backward direction for one time
@@ -49,4 +57,7 @@ class BidirectionalCell:
         Returns.
             The previous hidden state
         """
-        pass
+
+        con = np.concatenate((h_next, x_t), axis=1)
+
+        return np.tanh(np.matmul(con, self.Whb) + self.bhb)
