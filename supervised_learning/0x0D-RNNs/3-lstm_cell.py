50dd58e2a50d25bb85f61d66f0455ce78b087b9b
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-02 17:03:05 -0500

    class that represents an LSTM unit

--- supervised_learning/0x0D-RNNs/3-lstm_cell.py
+++ supervised_learning/0x0D-RNNs/3-lstm_cell.py
@@ -17,7 +17,18 @@ class LSTMCell:
             h: dimensionality of the hidden state
             o: dimensionality of the outputs
         """
-        pass
+
+        self.Wf = np.random.randn(i + h, h)
+        self.Wu = np.random.randn(i + h, h)
+        self.Wc = np.random.randn(i + h, h)
+        self.Wo = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
+        self.bf = np.zeros((1, h))
+        self.bu = np.zeros((1, h))
+        self.bc = np.zeros((1, h))
+        self.bo = np.zeros((1, h))
+        self.by = np.zeros((1, h))
+        self.bf = np.zeros((1, o))
 
     def forward(self, h_prev, c_prev, x_t):
         """Performs forward propagation for one time step
@@ -35,4 +46,15 @@ class LSTMCell:
             The next hidden state, the next cell state and the output of the
             cell
         """
-        pass
+
+        con = np.concatenate((h_prev, x_t), axis=1)
+        f_t = 1 / (1 + np.exp(-(np.matmul(con, self.Wf) + self.bf)))
+        u_t = 1 / (1 + np.exp(-(np.matmul(con, self.Wu) + self.bu)))
+        o_t = 1 / (1 + np.exp(-(np.matmul(con, self.Wo) + self.bo)))
+        c_t = np.tanh(np.matmul(con, self.Wc) + self.bc)
+        c_next = f_t * c_prev + u_t * c_t
+        h_next = o_t * np.tanh(c_next)
+        sof_mm = np.matmul(h_next, self.Wy) + self.by
+        y = np.exp(sof_mm) / np.sum(np.exp(sof_mm), axis=1, keepdims=True)
+
+        return h_next, c_next, y
