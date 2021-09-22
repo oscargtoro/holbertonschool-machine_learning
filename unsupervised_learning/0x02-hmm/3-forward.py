8b59ab72e3a3dc687b240fc296908b694673dfa5
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-22 17:05:24 -0500

    Performs the forward algorithm for a hidden markov model

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -30,4 +30,46 @@ def forward(Observation, Emission, Transition, Initial):
         of shape (N, T) containing the forward path probabilities, or None,
         None on failure
     """
-    return None, None
+
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return None, None
+
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+
+    T = Observation.shape[0]
+
+    N, _ = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    F = np.zeros((N, T))
+
+    F[:, 0] = Initial.transpose() * Emission[:, Observation[0]]
+
+    for i in range(1, T):
+        F[:,
+          i] = np.matmul(F[:, i - 1], Transition) * Emission[:, Observation[i]]
+
+    P = np.sum(F[:, T - 1])
+
+    return P, F
