aae15c8e9f511a8ebc48f5698f012f039404fd99
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-22 17:08:21 -0500

    Calculates the most likely sequence of hidden states for a hidden markov model

--- unsupervised_learning/0x02-hmm/4-viterbi.py
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -3,6 +3,7 @@
 """
 
 import numpy as np
+from numpy.lib.arraysetops import isin
 
 
 def viterbi(Observation, Emission, Transition, Initial):
@@ -31,4 +32,62 @@ def viterbi(Observation, Emission, Transition, Initial):
         and the probability of obtaining the path sequence, or None, None on
         failure
     """
-    return None, None
+
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return None, None
+
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+
+    T = Observation.shape[0]
+
+    N, _ = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    viterbi = np.zeros((N, T))
+    backpointer = np.zeros((N, T))
+
+    viterbi[:, 0] = Initial.transpose() * Emission[:, Observation[0]]
+
+    for t in range(1, T):
+        a = viterbi[:, t - 1]
+        b = Transition.transpose()
+        ab = a * b
+        ab_max = np.amax(ab, axis=1)
+        c = Emission[:, Observation[t]]
+        prob = ab_max * c
+
+        viterbi[:, t] = prob
+        backpointer[:, t - 1] = np.argmax(ab, axis=1)
+
+    path = []
+    current = np.argmax(viterbi[:, T - 1])
+    path = [current] + path
+
+    for t in range(T - 2, -1, -1):
+        current = int(backpointer[current, t])
+        path = [current] + path
+
+    P = np.amax(viterbi[:, T - 1], axis=0)
+
+    return path, P
