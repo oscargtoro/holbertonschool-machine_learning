047a9a135ee186b5428fc50cf8c9a64d57139953
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-22 17:12:14 -0500

    Performs the backward algorithm for a hidden markov model

--- unsupervised_learning/0x02-hmm/5-backward.py
+++ unsupervised_learning/0x02-hmm/5-backward.py
@@ -30,4 +30,46 @@ def backward(Observation, Emission, Transition, Initial):
         of shape (N, T) containing the backward path probabilities, or None,
         None on failure
     """
-    return None, None
+
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return None, None
+
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+
+    T = Observation.shape[0]
+
+    N, _ = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    B = np.zeros((N, T))
+    B[:, T - 1] = np.ones(N)
+    for t in range(T - 2, -1, -1):
+        prob = np.sum(B[:, t + 1] * Emission[:, Observation[t + 1]] *
+                      Transition,
+                      axis=1)
+        B[:, t] = prob
+
+    P = np.sum(Initial[:, 0] * Emission[:, Observation[0]] * B[:, 0])
+
+    return P, B
