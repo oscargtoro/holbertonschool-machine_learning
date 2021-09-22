df2f7bdba9f798667a9999a792980c8f0d2f5a7a
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-22 17:01:57 -0500

    Determines if a markov chain is absorbing

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -17,4 +17,32 @@ def absorbing(P):
     Returns.
         True if it is absorbing, or False on failure
     """
-    return False
+
+    if type(P) is not np.ndarray or len(P.shape) != 2:
+        return False
+
+    rows, columns = P.shape
+
+    if rows != columns:
+        return False
+
+    if np.sum(P, axis=1).all() != 1:
+        return False
+
+    D = np.diagonal(P)
+
+    if np.all(D == 1):
+        return True
+
+    if not np.any(D == 1):
+        return False
+
+    count = np.count_nonzero(D == 1)
+    Q = P[count:, count:]
+    Id = np.eye(Q.shape[0])
+
+    try:
+        if (np.any(np.linalg.inv(Id - Q))):
+            return True
+    except np.linalg.LinAlgError:
+        return False
