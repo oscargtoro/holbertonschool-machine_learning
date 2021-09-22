335701ffb100920b8b1ceff99b2d7532c99fe502
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-22 17:18:56 -0500

    Removed imports, added imports code

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -4,9 +4,6 @@
 
 import numpy as np
 
-forward = __import__('3-forward').forward
-backward = __import__('5-backward').backward
-
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
     """Performs the Baum-Welch algorithm for a hidden markov model
@@ -100,3 +97,143 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
         Emission = Emission / denominator
 
     return Transition, Emission
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """Performs the forward algorithm for a hidden markov model
+
+    Args.
+        Observation: a numpy.ndarray of shape (T,) that contains the index of
+        the observation
+            -T is the number of observations
+        Emission: a numpy.ndarray of shape (N, M) containing the emission
+        probability of a specific observation given a hidden state
+            -Emission[i, j] is the probability of observing j given the hidden
+             state i
+            -N is the number of hidden states
+            -M is the number of all possible observations
+        Transition: is a 2D numpy.ndarray of shape (N, N) containing the
+        transition probabilities
+            -Transition[i, j] is the probability of transitioning from the
+             hidden state i to j
+        Initial: a numpy.ndarray of shape (N, 1) containing the probability of
+        starting in a particular hidden state
+
+    Returns:
+        The likelihood of the observations given the model and a numpy.ndarray
+        of shape (N, T) containing the forward path probabilities, or None,
+        None on failure
+    """
+
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return None, None
+
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+
+    T = Observation.shape[0]
+
+    N, _ = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    F = np.zeros((N, T))
+
+    F[:, 0] = Initial.transpose() * Emission[:, Observation[0]]
+
+    for i in range(1, T):
+        F[:,
+          i] = np.matmul(F[:, i - 1], Transition) * Emission[:, Observation[i]]
+
+    P = np.sum(F[:, T - 1])
+
+    return P, F
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """Performs the backward algorithm for a hidden markov model
+
+    Args.
+        Observation: is a numpy.ndarray of shape (T,) that contains the index
+        of the observation
+            -T is the number of observations
+        Emission: is a numpy.ndarray of shape (N, M) containing the emission
+        probability of a specific observation given a hidden state
+            -Emission[i, j] is the probability of observing j given the hidden
+            state i
+            -N is the number of hidden states
+            -M is the number of all possible observations
+        Transition: is a 2D numpy.ndarray of shape (N, N) containing the
+        transition probabilities
+            -Transition[i, j] is the probability of transitioning from the
+             hidden state i to j
+        Initial: a numpy.ndarray of shape (N, 1) containing the probability of
+        starting in a particular hidden state
+
+    Returns: P, B, or None, None on failure
+        The likelihood of the observations given the model and a numpy.ndarray
+        of shape (N, T) containing the backward path probabilities, or None,
+        None on failure
+    """
+
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return None, None
+
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+
+    T = Observation.shape[0]
+
+    N, _ = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    B = np.zeros((N, T))
+    B[:, T - 1] = np.ones(N)
+    for t in range(T - 2, -1, -1):
+        prob = np.sum(B[:, t + 1] * Emission[:, Observation[t + 1]] *
+                      Transition,
+                      axis=1)
+        B[:, t] = prob
+
+    P = np.sum(Initial[:, 0] * Emission[:, Observation[0]] * B[:, 0])
+
+    return P, B
