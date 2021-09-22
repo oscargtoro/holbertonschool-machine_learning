4f591067c2bb10e86ecde206fb6da4aaf162cf41
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-22 16:13:34 -0500

    Completed the code for a class that represents a noiseless 1D Gaussian process

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -8,7 +8,6 @@ import numpy as np
 class GaussianProcess:
     """Represents a noiseless 1D Gaussian process
     """
-
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """Contructor definition
 
@@ -22,7 +21,11 @@ class GaussianProcess:
             black-box function
         """
 
-        pass
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """Calculates the covariance kernel matrix between two matrices
@@ -34,4 +37,6 @@ class GaussianProcess:
             The covariance kernel matrix as a numpy.ndarray
         """
 
-        pass
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) \
+            + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
