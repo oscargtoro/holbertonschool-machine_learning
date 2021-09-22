353be86272748ed49c9bbda1d9111b9c752fdc1a
Oscar Gomez Toro <oscarstiven87@gmail.com>
2021-09-22 16:22:13 -0500

    Updates a Gaussian Process

--- unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
@@ -8,7 +8,6 @@ import numpy as np
 class GaussianProcess:
     """Represents a noiseless 1D Gaussian process
     """
-
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """Contructor definition
 
@@ -22,7 +21,11 @@ class GaussianProcess:
             black-box function
         """
 
-        pass
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """Calculates the covariance kernel matrix between two matrices
@@ -34,7 +37,9 @@ class GaussianProcess:
             The covariance kernel matrix as a numpy.ndarray
         """
 
-        pass
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) \
+            + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
 
     def predict(self, X_s):
         """Predicts the mean and standard deviation of points in a Gaussian
@@ -48,7 +53,16 @@ class GaussianProcess:
             point in X_s
         """
 
-        pass
+        K = self.K
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(K)
+        mu_s = K_s.T.dot(K_inv).dot(self.Y)
+        mu_s = mu_s.reshape(-1)
+        cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
+        var_s = np.diag(cov_s)
+
+        return mu_s, var_s
 
     def update(self, X_new, Y_new):
         """Updates a Gaussian Process
@@ -59,4 +73,8 @@ class GaussianProcess:
             value
         """
 
-        pass
+        self.X = np.append(self.X, X_new)
+        self.X = self.X[:, np.newaxis]
+        self.Y = np.append(self.Y, Y_new)
+        self.Y = self.Y[:, np.newaxis]
+        self.K = self.kernel(self.X, self.X)
