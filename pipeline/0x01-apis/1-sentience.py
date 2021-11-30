6a022deb5a41783c10005e1f4a3ea2a9c465ad5c
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-11-30 12:44:16 -0500

    Update 1-sentience.py
    Fixed not processing the last page.

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -7,6 +7,27 @@ Module for the function(s):
 import requests
 
 
+def getSentientPlanets(url, planets=[]):
+    '''
+    Queries a url and populates a list.
+
+    Args.
+        url: url to query.
+        planets: list to populate.
+    '''
+
+    r = requests.get(url).json()
+    for species in r['results']:
+        sentient = [species['classification'], species['designation']]
+        if 'sentient' in sentient:
+            if species['homeworld'] is None or \
+                species['homeworld'] in planets:
+                continue
+            homeworld = requests.get(species['homeworld']).json()['name']
+            planets.append(homeworld)
+    if not isinstance(r['next'], type(None)):
+        getSentientPlanets(r['next'], planets)
+
 def sentientPlanets():
     '''
     Queries the Swapi API for names of the home planets of all sentient species
@@ -15,17 +36,7 @@ def sentientPlanets():
         A list with the names of the home planets of all sentient species.
     '''
 
-    url = 'https://swapi-api.hbtn.io/api/species/'
-    r = requests.get(url).json()
     planets = []
-
-    while r['next'] is not None:
-        for species in r['results']:
-            if species['designation'] == 'sentient':
-                if species['homeworld'] is None or species['homeworld'] in planets:
-                    continue
-                homeworld = requests.get(species['homeworld']).json()['name']
-                planets.append(homeworld)
-        r = requests.get(r['next']).json()
+    getSentientPlanets('https://swapi-api.hbtn.io/api/species/', planets)
 
     return planets
