0039c7a4467e8f766362063aca614129ffcbde7f
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-02 09:38:10 -0500

    Update 2-user_location.py
    Removed usage of datetime and removed usage of f-strings

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -12,9 +12,10 @@ if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     if r.status_code == requests.codes.not_found:
         print('Not found')
-    # if r.status_code == requests.codes.forbidden:
-    #     window_start = int(time.time())
-    #     window_end = int(r.headers["X-Ratelimit-Reset"])
-    #     print(f'Reset in {(window_end - window_start) // 60} min')
+    if r.status_code == requests.codes.forbidden:
+        window_start = int(time.time())
+        window_end = int(r.headers['X-Ratelimit-Reset'])
+        # print(f'Reset in {(window_end - window_start) // 60} min')
+        print('Reset in {} min'.format((window_end - window_start) // 60))
     if r.status_code == requests.codes.ok:
         print(r.json()['location'])
