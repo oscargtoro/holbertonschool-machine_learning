560f77cca2a0b8fb27193f09db2f966dc69077c6
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-02 09:16:46 -0500

    Update 2-user_location.py
    Added datetime, this time importing the complete module

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -5,15 +5,16 @@ Script that prints the location of a github user
 
 import requests
 import sys
+import datetime
 
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     if r.status_code == requests.codes.not_found:
         print('Not found')
-    # if r.status_code == requests.codes.forbidden:
-    #     window_start = int(datetime.now().timestamp())
-    #     window_end = int(r.headers["X-Ratelimit-Reset"])
-    #     print(f'Reset in {(window_end - window_start) // 60} min')
+    if r.status_code == requests.codes.forbidden:
+        window_start = int(datetime.datetime.now().timestamp())
+        window_end = int(r.headers["X-Ratelimit-Reset"])
+        print(f'Reset in {(window_end - window_start) // 60} min')
     if r.status_code == requests.codes.ok:
         print(r.json()['location'])
