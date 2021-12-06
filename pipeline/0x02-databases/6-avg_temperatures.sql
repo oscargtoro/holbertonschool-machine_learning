11471edddf480af271fc1023e74cd630f6b3608e
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-06 14:10:57 -0500

    Create 6-avg_temperatures.sql
    Importing temperatures.sql is necessary for this script.
    Displays the average temperature (Fahrenheit) by city ordered by temperature (descending).

---
+++ pipeline/0x02-databases/6-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- Importing temperatures.sql is necessary for this script
+-- Displays the average temperature (Fahrenheit) by city ordered by
+-- temperature (descending).
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
