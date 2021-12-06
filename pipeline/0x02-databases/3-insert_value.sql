db395457facc2352aac02582673a9835f3f743f6
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-06 13:39:31 -0500

    Create 3-insert_value.sql
    Inserts a new row in the table first_table in your MySQL server.
    The database name must be passed as an argument of the mysql command.

---
+++ pipeline/0x02-databases/3-insert_value.sql
@@ -0,0 +1,4 @@
+-- Inserts a new row in the table first_table in your MySQL server.
+-- The database name must be passed as an argument of the mysql command.
+
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
