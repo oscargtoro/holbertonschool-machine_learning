8ae7bfeab96b5525c45bb714804a2e6b3951b380
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 10:02:53 -0500

    Update 15-fans.sql
    Ranks country origins of bands, ordered by the number of (non-unique) fans.
    Uses the metal_bands.sql dump

--- pipeline/0x02-databases/15-fans.sql
+++ pipeline/0x02-databases/15-fans.sql
@@ -1,1 +1,7 @@
--- empty task file
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+-- Uses the metal_bands.sql dump
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC
