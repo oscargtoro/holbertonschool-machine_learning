d6aa59456538ac0d06d575200e05b95ad83bc45e
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 09:26:36 -0500

    Update 12-rating_genres.sql
    Lists all genres in the database hbtn_0d_tvshows_rate by their rating, results are sorted in descending order by their rating.
    The database name must be passed as an argument of the mysql command.

--- pipeline/0x02-databases/12-rating_genres.sql
+++ pipeline/0x02-databases/12-rating_genres.sql
@@ -1,1 +1,11 @@
--- empty task file
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating,
+-- results are sorted in descending order by their rating.
+-- The database name must be passed as an argument of the mysql command.
+
+SELECT g.name, SUM(sr.rate) AS rating
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+INNER JOIN tv_shows AS s ON sg.show_id = s.id
+INNER JOIN tv_show_ratings AS sr ON s.id = sr.show_id
+GROUP BY g.name
+ORDER BY rating DESC
