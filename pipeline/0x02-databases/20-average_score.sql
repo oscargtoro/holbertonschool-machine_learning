9316a3236491c9cd47783a70fd7ef89b52c1928d
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 15:24:07 -0500

    Update 20-average_score.sql
    Procedure that computes and stores the average score for a student.
    Uses the DB structure in 20-init.sql.

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -1,1 +1,12 @@
--- empty task file
+-- Procedure that computes and stores the average score for a student.
+-- Uses the DB structure in 20-init.sql.
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+    SET @scr_avg = (SELECT AVG(c.score)
+                    FROM corrections AS c
+                    WHERE c.user_id = user_id);
+    UPDATE users SET average_score = @scr_avg where id = user_id;
+END//
+DELIMITER ;
