15f73ee574b47468d55b2c9e0cb71fe46db801a7
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-06 21:02:35 -0500

    Wrong JOIN used
    Changed LEFT JOIN with INNER JOIN since genres with no shows linked aren't displayed

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -5,6 +5,6 @@
 
 SELECT g.name as genre, COUNT(sg.show_id) as number_of_shows
 FROM tv_genres AS g
-LEFT JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+INNER JOIN tv_show_genres AS sg ON g.id = sg.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC
