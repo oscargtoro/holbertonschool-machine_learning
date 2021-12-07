39cc89ecc404224c92932a6aa2b5e0e0f19ff55a
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 13:29:34 -0500

    Update 19-bonus.sql
    Stored procedure AddBonus that adds a new correction for a student.
    Uses the structure in 19-init.sql.

--- pipeline/0x02-databases/19-bonus.sql
+++ pipeline/0x02-databases/19-bonus.sql
@@ -1,1 +1,14 @@
--- empty task file
+-- Stored procedure AddBonus that adds a new correction for a student.
+-- Uses the structure in 19-init.sql.
+
+DELIMITER //
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    SET @project_id = (SELECT id FROM projects WHERE name = project_name);
+    IF @project_id IS NULL THEN
+        INSERT INTO projects(name) VALUES (project_name);
+        SET @project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) values (user_id, @project_id, score);
+END//
+DELIMITER ;
