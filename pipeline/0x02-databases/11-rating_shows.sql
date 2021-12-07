49620887ebac251d49ad07c6a1b1e40a2ac2c250
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 08:58:34 -0500

    Update 11-rating_shows.sql
    Lists all shows from hbtn_0d_tvshows_rate by their rating, sorted in descending order by rating.
    The database name must be passed as an argument of the mysql command.

--- pipeline/0x02-databases/11-rating_shows.sql
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -1,1 +1,9 @@
--- empty task file
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating, sorted in
+-- descending order by rating.
+-- The database name must be passed as an argument of the mysql command.
+
+SELECT s.title, SUM(sr.rate) AS rating
+FROM tv_shows AS s
+JOIN tv_show_ratings AS sr ON s.id = sr.show_id
+GROUP BY s.title
+ORDER BY rating DESC
