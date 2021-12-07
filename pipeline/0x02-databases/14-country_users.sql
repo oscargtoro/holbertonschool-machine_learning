15d78b4eed408494c90d1638196f24e66efbfd6f
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 09:47:15 -0500

    Update 14-country_users.sql
    Creates a table users With the attributes:
    id, integer, never null, auto increment and primary key
    email, string (255 characters), never null and unique
    name, string (255 characters)
    country, enumeration of countries: US, CO and TN, never null

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -1,1 +1,12 @@
--- empty task file
+-- Creates a table users With the attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+)
