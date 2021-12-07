2ef816d1723d0b39e1e18a917fac2d02d6f2f6b3
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 11:48:48 -0500

    Update 18-valid_email.sql
    Resets the attribute valid_email only when the email has been changed.
    Requires to first run 18-init.sql, 18-main.sql can be used for testing

--- pipeline/0x02-databases/18-valid_email.sql
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -1,1 +1,6 @@
--- empty task file
+-- Resets the attribute valid_email only when the email has been changed.
+-- Requires to first run 18-init.sql, 18-main.sql can be used for testing
+
+CREATE TRIGGER email_reset BEFORE UPDATE ON users
+FOR EACH ROW
+    SET NEW.valid_email = IF(NEW.email <> OLD.email, 0, NEW.valid_email)
