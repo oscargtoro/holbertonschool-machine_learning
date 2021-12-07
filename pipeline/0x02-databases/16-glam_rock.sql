5c5107c8984887a945fef60ff98ce903d7679683
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 10:50:29 -0500

    Update 16-glam_rock.sql
    Lists all bands with Glam rock as their main style, ranked by their longevity.
    Uses the metal_bands.sql dump

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -1,1 +1,9 @@
--- empty task file
+-- Lists all bands with Glam rock as their main style, ranked by their
+-- longevity.
+-- Uses the metal_bands.sql dump
+
+SELECT band_name,
+    IF(split IS NULL, 2020 - formed, split - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
