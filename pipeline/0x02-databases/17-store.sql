936895603f84605501afefec8d26ce547462ce5d
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-07 11:10:57 -0500

    Update 17-store.sql
    Trigger that decreases the quantity of an item after adding a new order.
    Requires to first run 17-init.sql, 17-main.sql can be used for testing

--- pipeline/0x02-databases/17-store.sql
+++ pipeline/0x02-databases/17-store.sql
@@ -1,1 +1,7 @@
--- empty task file
+-- Trigger that decreases the quantity of an item after adding a new order.
+-- Requires to first run 17-init.sql, 17-main.sql can be used for testing
+
+CREATE TRIGGER items_sub BEFORE INSERT ON orders
+    FOR EACH ROW
+    UPDATE items SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name
