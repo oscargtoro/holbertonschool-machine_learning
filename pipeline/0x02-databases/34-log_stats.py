aa9439413ffdcfe6e84ddc796e3ec956d20b7109
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-09 17:07:51 -0500

    Script that provides some stats about Nginx logs stored in MongoDB
    mongo dump used too heavy to upload

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -2,3 +2,21 @@
 '''
 Script that provides some stats about Nginx logs stored in MongoDB.
 '''
+
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(collection.estimated_document_count())
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        method_count = collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {method_count}')
+    status_count = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print(f'{status_count} status check')
