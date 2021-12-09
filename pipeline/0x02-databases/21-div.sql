2e07517071999ce6262ef97646e563b6e952f43e
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-09 04:37:21 -0500

    Update 21-div.sql
     Creates a function that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
    Uses the DB structure in 21-init.sql.

--- pipeline/0x02-databases/21-div.sql
+++ pipeline/0x02-databases/21-div.sql
@@ -1,1 +1,8 @@
--- empty task file
+-- Creates a function that divides (and returns) the first by the second
+-- number or returns 0 if the second number is equal to 0.
+-- Uses the DB structure in 21-init.sql.
+
+
+CREATE FUNCTION SafeDiv (a DOUBLE PRECISION, b DOUBLE PRECISION)
+RETURNS FLOAT DETERMINISTIC
+RETURN IF(b <> 0, a / b, 0);
