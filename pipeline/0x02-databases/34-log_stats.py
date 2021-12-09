0a14621fe753379e920d5c3a2e6ad17324580abd
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-09 17:15:02 -0500

    Removed use of f-strings

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    print(f'{collection.estimated_document_count()} logs')
+    print('{} logs'.format(collection.estimated_document_count()))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         method_count = collection.count_documents({'method': method})
-        print(f'\tmethod {method}: {method_count}')
+        print('\tmethod {}: {}'.format(method, method_count))
     status_count = collection.count_documents(
         {"method": "GET", "path": "/status"}
         )
-    print(f'{status_count} status check')
+    print('{} status check'.format(status_count))
