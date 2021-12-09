c71b28b658bed40ed1936424a9bcdc5f40fd9130
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2021-12-09 17:13:23 -0500

    Update 34-log_stats.py
    Restored use of f-strings
    Missing word 'logs' on count print

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    print(collection.estimated_document_count())
+    print(f'{collection.estimated_document_count()} logs')
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         method_count = collection.count_documents({'method': method})
-        print('\tmethod {}: {}'.format(method, method_count))
+        print(f'\tmethod {method}: {method_count}')
     status_count = collection.count_documents(
         {"method": "GET", "path": "/status"}
         )
-    print('{} status check'.format(status_count))
+    print(f'{status_count} status check')
